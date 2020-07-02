6e5bca005a517ca0f99e160a8bf975e797503364
Vanessa P <vanessa.pereira.o@hotmail.com>
2020-07-01 23:45:37 -0300

    Primeiro commit - Adição de conteúdo

---
+++ api.js
@@ -0,0 +1,4 @@
+let apod = new XMLHttpRequest();
+apod.open =("GET", 'https://api.nasa.gov/planetary/apod?api_key=iVkNoOjEOuexT2chsygtVGsKxwFgH1SoWzoPkBOz');
+                
+                    apod.send();
