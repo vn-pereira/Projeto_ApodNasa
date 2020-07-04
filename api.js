1bca94d7ac1952c4dc14d15126f43c19e6b7dfeb
Vanessa P <vanessa.pereira.o@hotmail.com>
2020-07-03 22:03:52 -0300

    Alteração de conteudo e adição de CSS

--- api.js
+++ api.js
@@ -1,4 +1,25 @@
-let apod = new XMLHttpRequest();
-apod.open =("GET", 'https://api.nasa.gov/planetary/apod?api_key=iVkNoOjEOuexT2chsygtVGsKxwFgH1SoWzoPkBOz');
+let apod = new XMLHttpRequest(); // criando o obj que faz a requisição
+apod.open("GET", 'https://api.nasa.gov/planetary/apod?api_key=iVkNoOjEOuexT2chsygtVGsKxwFgH1SoWzoPkBOz'); //abrindo req
+
+       // TRATAR OS EVENTOS ANTES DE ENVIAR A REQUISIÇÃO !!!
+    apod.onload = function (){ // essa função anonima é callback 
+        if(this.status == 200){
+            let objeto = JSON.parse(apod.responseText);
+                console.log(objeto.copyright);
+        
+            let data = document.getElementById('data');
+            data.innerText = ` Data ${objeto.date}`;
+
+            let foto = document.getElementById('imagem');
+            foto.src = objeto.url;
+        
+            let rodape = document.getElementById('rodape');
+            rodape.innerText = `Image Credit & Copyright: ${objeto.copyright}`
+
+            let legenda = document.getElementById('legenda');
+            legenda.innerText = objeto.explanation;
+     }
+        
+}
                 
-                    apod.send();
+apod.send();                   
